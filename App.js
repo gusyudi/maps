42aa2039d67e9f0f668851e272abb5cec60cee43
gusyudi <37138411+gusyudi@users.noreply.github.com>
2018-06-03 21:26:02 +0800

    maps

---
+++ App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+export default class App extends Component {
+
+  state = {
+    region: {
+      latitude: -8.149407,
+      longitude: 115.216667,
+      latitudeDelta: 0.8922,
+      longitudeDelta: 0.8421,
+    },
+    markers : [
+      {
+        key:1,
+        latlng: {
+          latitude:-8.115583,
+          longitude: 115.089317
+        },
+        title: 'Warung Nasi Pecel Bu Lina',
+        subtitle: ''
+      },
+      {
+        key:2,
+        latlng: {
+          latitude:-8.118862,
+          longitude:  115.090832,
+        },
+        title: 'Warung Makan Scuniex',
+        subtitle: ''
+      },
+      {
+        key:3,
+        latlng: {
+          latitude:-8.116388, 
+          longitude: 115.095081
+        },
+        title: 'WARUNG MAKAN PRASADAM'
+      }
+      ,
+      {
+        key:4,
+        latlng: {
+          latitude:-8.115933, 
+          longitude: 115.094900
+        },
+        title: 'Warung Makan Bakso'
+      }
+   ]
+  };
+
+  render() {
+    return (
+      <View style={styles.contMain}>
+        <View style={styles.contHeader}>
+            <Text style={styles.textHeader}>
+              warung makan singaraja
+            </Text>
+        </View>
+        <View style={styles.contMaps}>
+              <MapView
+                style={styles.map}
+                region={this.state.region}
+              >
+              {this.state.markers.map(mark => (
+              <Marker
+                  key = {mark.key}
+                  coordinate={mark.latlng}
+                  title={mark.title}
+                  description={mark.subtitle}
+                />
+              ))}
+              </MapView>
+        </View>
+        <View style={styles.contFooter}>
+           <Text style={styles.textFooter}> http://whidigede@gmail.com </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  contMain: {
+      flex : 1
+  },
+  contHeader: {
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  contMaps : {
+    flex : 10
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contFooter: {
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  textFooter: {
+    fontSize: 16,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+});
